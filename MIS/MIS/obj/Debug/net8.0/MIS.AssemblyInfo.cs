fa1d6cd8b46b1909cd35c8d21726a491fb7d96dd
Mikhaylov Igor <mikhaylov61180@gmail.com>
2024-10-23 18:10:37 +0700

    Создание стартового проекта

--- MIS/MIS/obj/Debug/net8.0/MIS.AssemblyInfo.cs
+++ MIS/MIS/obj/Debug/net8.0/MIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8dd9c933d575bda6c939a7511a09958abd3311")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
